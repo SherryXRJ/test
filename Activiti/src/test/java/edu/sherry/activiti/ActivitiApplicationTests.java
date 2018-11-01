9dbce4bcf5a011432b962975a348e933fea7193e
xurunjie <xurunjie@migu.cn>
2018-11-01 18:10:23 +0800

    modify

--- Activiti/src/test/java/edu/sherry/activiti/ActivitiApplicationTests.java
+++ Activiti/src/test/java/edu/sherry/activiti/ActivitiApplicationTests.java
@@ -1,5 +1,10 @@
 package edu.sherry.activiti;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngines;
+import org.activiti.engine.repository.Deployment;
+import org.activiti.engine.repository.DeploymentQuery;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,8 +14,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ActivitiApplicationTests {
 
+
+    private ProcessEngine processEngine;
+
+    @Before
+    public void init(){
+        processEngine = ProcessEngines.getDefaultProcessEngine();
+
+    }
+
 	@Test
-	public void contextLoads() {
-	}
+	public void deploy() {
+        processEngine.getRepositoryService().createDeployment()
+        .name("deploy hello world")
+        .addClasspathResource("diagrams/helloWorld.bpmn")
+        .deploy();
+    }
+
+    @Test
+    public void deployQuery(){
+        Deployment result = processEngine.getRepositoryService()
+                .createDeploymentQuery()
+                .deploymentId("1")
+                .singleResult();
+        System.out.println(result.getName());
+    }
 
 }
