713dd26474be0c53be7401b29deb5d80e9ca191e
xurunjie <xurunjie@migu.cn>
2018-11-01 22:00:12 +0800

    update test

--- Activiti/src/test/java/edu/sherry/activiti/ActivitiApplicationTests.java
+++ Activiti/src/test/java/edu/sherry/activiti/ActivitiApplicationTests.java
@@ -4,11 +4,17 @@ import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngines;
 import org.activiti.engine.repository.Deployment;
 import org.activiti.engine.repository.DeploymentQuery;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,6 +35,34 @@ public class ActivitiApplicationTests {
         .name("deploy hello world")
         .addClasspathResource("diagrams/helloWorld.bpmn")
         .deploy();
+    }
+    /**
+     * 启动流程
+     */
+    @Test
+    public void start(){
+        ProcessInstance instance = processEngine.getRuntimeService().startProcessInstanceById("helloWordl:1:4");
+        System.out.println(instance.getDeploymentId());
+        System.out.println(instance.getName());
+    }
+
+    @Test
+    public void query(){
+        List<Task> list = processEngine.getTaskService().createTaskQuery()
+                .taskAssignee("王五")
+                .list();
+        list.forEach(task -> {
+            System.out.println(task.getAssignee());
+            System.out.println(task.getId());
+            System.out.println(task.getName());
+            System.out.println(task.getTaskLocalVariables().size());
+        });
+    }
+
+    @Test
+    public void complete(){
+        Map<String, Object> param = new HashMap<>();
+        processEngine.getTaskService().complete("7501");
     }
 
     @Test
