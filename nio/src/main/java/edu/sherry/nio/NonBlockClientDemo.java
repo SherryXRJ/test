7369d2b09d3bdbf8f9d20a20bf88c1c51d8b4a60
xurunjie <xurunjie@migu.cn>
2018-11-07 14:36:20 +0800

    non block

---
+++ nio/src/main/java/edu/sherry/nio/NonBlockClientDemo.java
@@ -0,0 +1,21 @@
+package edu.sherry.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+public class NonBlockClientDemo {
+
+    public static void main(String[] args) throws IOException {
+        SocketChannel clientChannel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 65530));
+        //  非阻塞模式
+        clientChannel.configureBlocking(false);
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        buffer.put("zxcvb".getBytes());
+        buffer.flip();
+        clientChannel.write(buffer);
+        clientChannel.close();
+    }
+
+}
---
+++ nio/src/main/java/edu/sherry/nio/NonBlockServerDemo.java
@@ -0,0 +1,51 @@
+package edu.sherry.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+
+public class NonBlockServerDemo {
+
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+        serverChannel.bind(new InetSocketAddress(65530));
+
+        Selector selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        while (selector.select() > 0) {
+
+            Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                if (key.isAcceptable()) {           //  可接收
+                    //  客户端通道
+                    SocketChannel clientChannel = serverChannel.accept();
+                    clientChannel.configureBlocking(false);
+                    clientChannel.register(selector, SelectionKey.OP_READ);
+                } else if (key.isReadable() && key.isValid()) {      //  可读取
+                    SocketChannel readableChannel = (SocketChannel) key.channel();//获取对应的通道
+                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+                    int length;
+                    while ((length = readableChannel.read(buffer)) > 0) {
+                        buffer.flip();
+                        System.out.println(new String(buffer.array(), 0, length));
+                    }
+                } else if (key.isConnectable()) {   //  可连接
+
+                } else if (key.isWritable()) {      //  可写
+
+                }
+                iterator.remove();
+            }
+        }
+
+    }
+
+}
