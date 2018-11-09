5200893abadd25e462ca57f712a920468c72313e
xurunjie <xurunjie@migu.cn>
2018-11-09 18:11:02 +0800

    modify

--- testcustomer/src/main/java/edu/sherry/demo/testcustomer/TestApplication.java
+++ testcustomer/src/main/java/edu/sherry/demo/testcustomer/TestApplication.java
@@ -2,9 +2,12 @@ package edu.sherry.demo.testcustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
-@EnableEurekaClient
+@ComponentScan(basePackages = {"edu.sherry.demo"})
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TestApplication {
 
--- testcustomer/src/main/java/edu/sherry/demo/testcustomer/controller/TestCustomerController.java
+++ testcustomer/src/main/java/edu/sherry/demo/testcustomer/controller/TestCustomerController.java
@@ -3,6 +3,7 @@ package edu.sherry.demo.testcustomer.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import redis.clients.jedis.ShardedJedis;
 
 import javax.annotation.Resource;
@@ -12,8 +13,11 @@ import javax.annotation.Resource;
 @RequestMapping("/testCustomer")
 public class TestCustomerController {
 
-    @Resource(name = "ShardedJedis")
+    @Resource(name = "shardedJedis")
     private ShardedJedis shardedJedis;
+
+    @Resource(name = "restTemplate")
+    private RestTemplate restTemplate;
 
     @GetMapping("/get")
     public String get() {
