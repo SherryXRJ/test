b8dd7bc1118d25f9b8614e6f2690ef0f15d4612c
xurunjie <xurunjie@migu.cn>
2018-11-12 18:23:33 +0800

    activemq

--- common/src/main/java/edu/sherry/demo/common/activemq/ActiveMQConfiguration.java
+++ common/src/main/java/edu/sherry/demo/common/activemq/ActiveMQConfiguration.java
@@ -1,8 +1,36 @@
 package edu.sherry.demo.common.activemq;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.pool.PooledConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.core.JmsTemplate;
 
+import java.util.Properties;
+
+
+/**
+ * 参考官网配置
+ * http://activemq.apache.org/spring-support.html
+ */
 @Configuration
 public class ActiveMQConfiguration {
 
+    /**
+     * ActiveMQ连接工厂
+     */
+    @Bean
+    public ActiveMQConnectionFactory activeMQConnectionFactory(){
+        return new ActiveMQConnectionFactory();
+    }
+
+    @Bean(destroyMethod = "stop")
+    public PooledConnectionFactory pooledConnectionFactory(ActiveMQConnectionFactory activeMQConnectionFactory){
+        return new PooledConnectionFactory(activeMQConnectionFactory);
+    }
+
+    @Bean public JmsTemplate jmsTemplate(PooledConnectionFactory pooledConnectionFactory){
+        return new JmsTemplate(pooledConnectionFactory);
+    }
 }
